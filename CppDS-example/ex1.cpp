922880b0daf79ba59c77fa613cba2dc9ed0f7c84
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-02 18:22:46 +0900

    add

--- CppDS-example/ex1.cpp
+++ CppDS-example/ex1.cpp
@@ -1,10 +1,20 @@
-/*#include <iostream>
+#include <iostream>
 
 using namespace std;
-int main(){
-    array<int,5> arr = {1,2,3,4,5};
+/*int main(){
+    array<,5> arr = {1,2,3,4,5};
     for(auto element : arr)
     cout<<element<<' ';
 
 
 }*/
+/*
+void print(int arr[5]){
+    for(auto ele = arr.be)
+    cout<<ele<<' ';
+}
+int main(){
+    array<int,5> arr = {1,2,3,4,5};
+    print(arr);
+}
+*/
