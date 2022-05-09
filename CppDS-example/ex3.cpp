e74e63ba750da8be1279e10a916714a8c9eb2687
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-09 12:57:48 +0900

    add

--- CppDS-example/ex3.cpp
+++ CppDS-example/ex3.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <list>
 using namespace std;
+
+struct doubly_linked_list_node
+{
+    int data;
+    doubly_linked_list_node *next; //앞 포인터
+    doubly_linked_list_node *prev; //뒷 포인터
+};
+
 int main()
 {
+    list<int> list1 = {1, 2, 3, 4, 5};
+    list1.push_back(6);
+    list1.insert(next(list1.begin()), 0); // fwdl와 비슷하게 insert(index,parameter)로 사용함
+    list1.insert(list1.end(), 7);         // end는 6바로 다음의 공간임 prev* = end()? ----- result list1 = {1,0,2,3,4,5,6,7};
+    list1.pop_back();                     // fwdl에서는 제공하지 않는 맨뒤를 제거하는 함수
 }
