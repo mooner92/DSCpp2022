19c6abc39d42f3c6d2492abd3f0c6b799202cd5f
Mooner92 <chlaudgjs0989@gmail.com>
2022-05-09 12:58:10 +0900

    add

--- CppDS-example/ex1.cpp
+++ CppDS-example/ex1.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
-
+/*#include <iostream>
 using namespace std;
-/*int main(){
+int main(){
     array<,5> arr = {1,2,3,4,5};
     for(auto element : arr)
     cout<<element<<' ';
@@ -18,3 +17,26 @@ int main(){
     print(arr);
 }
 */
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <forward_list>
+using namespace std;
+
+template <typename T>
+class dynamic_array
+{
+    T *data;
+    sizt_t n;
+
+public:
+    dynamic_array(int n)
+    {
+        this->n = n;
+        data = new T[n];
+    }
+};
+int main()
+{
+    forward_list<int> k = {1, 2, 3};
+}
